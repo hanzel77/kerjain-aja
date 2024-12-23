cd0adfe596da8c7aed8e8ff729982ce3a8d1c610
hanzel77 <hhanzel0810@gmail.com>
2024-12-23 19:55:00 +0700

    Adding dicoding meta tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="dicoding:email" content="hhanzel0810@gmail.com">
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
